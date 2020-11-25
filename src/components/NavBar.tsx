20d97ae84c8035f164f671ab3e378ecbf2050a75
bschoenfeld16 <9916245+bschoenfeld16@users.noreply.github.com>
2020-11-24 22:10:33 -0600

    cleaning up css

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,18 +7,15 @@ interface NavBarProps {}
 
 const NavBar: React.FC<NavBarProps> = ({}) => {
     return (
-        <>
-            <div className={`${classes.Navbar} ${classes.Blur}`} />
-            <header className={classes.Navbar}>
-                <NextLink href="/">
-                    <a className={classes.NavbarLink}>home</a>
-                </NextLink>
-                <NextLink href="/">
-                    <a className={classes.NavbarLink}>projects</a>
-                </NextLink>
-                <DarkModeToggle />
-            </header>
-        </>
+        <header className={classes.Navbar}>
+            <NextLink href="/">
+                <a className={classes.NavbarLink}>home</a>
+            </NextLink>
+            <NextLink href="/">
+                <a className={classes.NavbarLink}>projects</a>
+            </NextLink>
+            <DarkModeToggle />
+        </header>
     );
 };
 

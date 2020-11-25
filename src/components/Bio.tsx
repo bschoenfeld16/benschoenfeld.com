a1fb3ad5ff4f77a8bbdd9d8b13be7a1b475c1458
bschoenfeld16 <9916245+bschoenfeld16@users.noreply.github.com>
2020-11-24 22:40:56 -0600

    styling the about me section with image above

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -3,21 +3,26 @@ import Image from "next/image";
 import classes from "./Bio.module.css";
 
 const Bio: React.FC = ({}) => (
-    <div className={classes.Bio}>
-        <Image
-            className={classes.Image}
-            alt="Ben Schoenfeld"
-            src="/images/IMG_0019.jpeg"
-            width="128"
-            height="128"
-        />
-        <div className={classes.Info}>
-            Hi, I'm Ben. I'm a Software Engineer in the Minneapolis, MN area. I
-            mostly work on backend infrastructure in c++ and Java. I'm currently
-            interested in learning Next.js with GraphQL backend API's to develop
-            full stack web applications.
+    <>
+        <div className={classes.Image}>
+            <Image
+                className={classes.Image}
+                alt="Ben Schoenfeld"
+                src="/images/IMG_0019.jpeg"
+                width="128"
+                height="128"
+            />
         </div>
-    </div>
+        <div className={classes.Bio}>
+            <h1>About Me</h1>
+            <div className={classes.Info}>
+                Hi, I'm Ben. I'm a Software Engineer in the Minneapolis, MN
+                area. I mostly work on backend infrastructure in c++ and Java.
+                I'm currently interested in learning Next.js with GraphQL
+                backend API's to develop full stack web applications.
+            </div>
+        </div>
+    </>
 );
 
 export default Bio;

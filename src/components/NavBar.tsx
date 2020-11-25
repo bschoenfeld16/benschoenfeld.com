5d70a1dc3acae4d3254259d8055a640bc3a9bcd6
bschoenfeld16 <9916245+bschoenfeld16@users.noreply.github.com>
2020-11-25 11:42:10 -0600

    Adding ActiveLink component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import NextLink from "next/link";
 import classes from "./Navbar.module.css";
 import DarkModeToggle from "./DarkModeToggle";
+import ActiveLink from "./ActiveLink";
 
 interface NavBarProps {}
 
 const NavBar: React.FC<NavBarProps> = ({}) => {
     return (
         <header className={classes.Navbar}>
-            <NextLink href="/">
+            <ActiveLink activeClassName={classes.active} href="/">
                 <a className={classes.NavbarLink}>home</a>
-            </NextLink>
-            <NextLink href="/">
+            </ActiveLink>
+            <ActiveLink activeClassName={classes.active} href="/projects">
                 <a className={classes.NavbarLink}>projects</a>
-            </NextLink>
+            </ActiveLink>
             <DarkModeToggle />
         </header>
     );

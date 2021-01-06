2d8d36820e4afa469edf841a3a76efb1a05bb369
bschoenfeld16 <9916245+bschoenfeld16@users.noreply.github.com>
2021-01-05 20:40:39 -0600

    formatting projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -3,14 +3,17 @@ import { BiLinkExternal } from "react-icons/bi";
 import classes from "./Projects.module.css";
 
 const Projects: React.FC = ({}) => (
-    <div>
+    <div className={classes.Container}>
         <h1>Projects</h1>
         <div className={classes.Project}>
             <a target="_blank" href="https://lireddit-web.benschoenfeld.com">
                 LiReddit
                 <BiLinkExternal />
             </a>
-            <div>a simple reddit clone</div>
+            <div>
+                A simple Reddit clone created with Next.js, TypeScript, Node.js,
+                GraphQL, and Postgres.
+            </div>
         </div>
     </div>
 );
